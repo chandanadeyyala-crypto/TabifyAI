01ae666c17cae1f6c418bc0e1482e1dd84b7f823
Chandana Deyyala <chandanadeyyala@gmail.com>
2026-06-18 21:00:59 +0530

    Fix Vite config default export

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-const API_URL = import.meta.env.VITE_API_URL;
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
-export default API_URL;
+});
