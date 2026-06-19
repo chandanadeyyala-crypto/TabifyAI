6a4e265596ccb04588dccf657d30483eb3ccfc59
Chandana Deyyala <chandanadeyyala@gmail.com>
2026-06-19 16:08:46 +0530

    updated code

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -8,7 +8,7 @@ function Loading({ setScreen, setTabs, audioFile }) {
   useEffect(() => {
     const fetchProgress = async () => {
       try {
-        const res = await fetch('${import.meta.env.VITE_API_URL}/progress');
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/progress`);
         const data = await res.json();
         const backendProgress = Number(data.progress);
         
@@ -44,7 +44,7 @@ function Loading({ setScreen, setTabs, audioFile }) {
     const formData = new FormData();
     formData.append("file", audioFile);
 
-    const uploadRes = await fetch('${import.meta.env.VITE_API_URL}/upload', {
+    const uploadRes = await fetch(  `${import.meta.env.VITE_API_URL}/upload`, {
       method: "POST",
       body: formData,
     });
@@ -54,9 +54,7 @@ function Loading({ setScreen, setTabs, audioFile }) {
     await fetchProgress();
 
     const tabRes = await fetch(
-      `${import.meta.env.VITE_API_URL}/generate-tabs?file_path=${encodeURIComponent(
-      uploadData.guitar_file
-      )}`,
+      `${import.meta.env.VITE_API_URL}/generate-tabs?file_path=${encodeURIComponent(uploadData.guitar_file)}`,
         {
           method: "POST",
         }
